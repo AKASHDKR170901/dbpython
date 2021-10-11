4f64e84e9428b49380632e1bf9e0dcf3ea97ce20
Akash <akashdkr170901@gmail.com>
2021-10-11 19:02:31 +0530

    done

--- 182note.py
+++ 182note.py
@@ -2,7 +2,10 @@ import cx_Oracle
 try:
     con=cx_Oracle.connect('scott/tiger@localhost')
     cursor=con.cursor()
-    cursor.execute(query,records)
+    increment=float(input('Enter increment salary:'))
+    salrange=float(input('Enter salary range:'))
+    query="update employees set esal=esal+%f where esal<%f"
+    cursor.execute(query%(increment,salrange))
     con.commit()
     print('Recorded successfully')
 except cx_Oracle.DatabaseError as e:
